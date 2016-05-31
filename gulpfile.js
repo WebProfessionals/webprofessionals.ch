d479a0a7b71e19753144585e663f895858bac1a4
Florian Gächter <florian.gaechter@gmail.com>
2016-05-31 19:49:48 +0200

    Add material design library

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,17 @@ var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 var argv = require('yargs').argv;
 
-elixir.config.assetsPath = 'source/_assets';
+elixir.config.assetsPath = 'source/_assets/';
 elixir.config.publicPath = 'source';
 
 elixir(function(mix) {
     var env = argv.e || argv.env || 'local';
 
-    mix.sass('main.scss')
+    mix
+      .sass([
+        '../vendor/material-design-lite/src/material-design-lite.scss',
+        'main.scss'
+      ], 'source/css/main.css')
         .exec('jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
         .browserSync({
             server: { baseDir: 'build_' + env },
---
+++ source/lehrgaenge/index.md
@@ -0,0 +1,8 @@
+---
+extends: _layouts.master
+section: body
+---
+
+# My awesome heading!
+
+My awesome content!
