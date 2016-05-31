0dfb139350248aec440b74ef14bb4a065fd2da30
Florian Gächter <florian.gaechter@gmail.com>
2016-05-31 20:01:18 +0200

    Add js

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,20 @@ elixir.config.assetsPath = 'source/_assets/';
 elixir.config.publicPath = 'source';
 
 elixir(function(mix) {
-    var env = argv.e || argv.env || 'local';
+  var env = argv.e || argv.env || 'local';
 
-    mix
-      .sass([
-        '../vendor/material-design-lite/src/material-design-lite.scss',
-        'main.scss'
-      ], 'source/css/main.css')
-        .exec('jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
-        .browserSync({
-            server: { baseDir: 'build_' + env },
-            proxy: null,
-            files: [ 'build_' + env + '/**/*' ]
-        });
+  mix
+    .sass([
+      '../vendor/material-design-lite/src/material-design-lite.scss',
+      'main.scss'
+    ], 'source/css/main.css')
+    .scripts([
+      '../vendor/material-design-lite/material.min.js'
+    ], 'source/js/main.js')
+    .exec('jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
+    .browserSync({
+      server: { baseDir: 'build_' + env },
+      proxy: null,
+      files: [ 'build_' + env + '/**/*' ]
+    });
 });
--- source/index.blade.php
+++ source/index.blade.php
@@ -2,4 +2,8 @@
 
 @section('body')
 <h1>Hello world!</h1>
+<!-- Accent-colored raised button with ripple -->
+<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+  Button
+</button>
 @endsection
