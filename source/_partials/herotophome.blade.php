b801e32471dfa41e77a28388b9c46ae25af1fff3
Rolf Eggenberger <rolf@kunstwort.com>
2016-08-17 11:39:52 +0200

    SoMe Icons

--- source/_partials/herotophome.blade.php
+++ source/_partials/herotophome.blade.php
@@ -1,14 +1,7 @@
 <div class="row">
-    <div class="col s12 m12 l12 center-align webpro-hero-logo"><a href="/index.html"><img src="/images/logo_web_professionals_top.svg" alt="Web Professionals Logo" title="Web Professionals Logo"></a></div>
-
+    <div class="col s12 m12 l12 center-align webpro-hero-logo"><a href="/index.html"><img src="/images/logo_web_professionals_top.svg" alt="Web Professionals Logo" title="Web Professionals Logo" width="400px"></a></div>
     <div class="col s12 m6 l6 right-align webpro-hero-social">
-        <div id="social-media">
-            <a href="https://play.google.com/store/apps/details?id=com.techfabrik.webpro" class="social-android" title="Android App der Web Professionals herunterladen" target="_blank">Android</a>
-            <a href="https://itunes.apple.com/app/id956778225" class="social-ios" title="iOS App der Web Professionals herunterladen" target="_blank">iOS</a>
-            <a href="https://www.facebook.com/WebProfessionalsOstschweiz" class="social-fb" title="Folge uns auf Facebook" target="_blank">Facebook</a>
-            <a href="https://twitter.com/WebProCH" class="social-tw" title="Unsere Tweets" target="_blank">Twitter</a>
-            <a href="https://plus.google.com/101281770968706437459/posts" class="social-gp" title="Google+ gibt es auch noch" target="_blank">Google+</a>
-            <a href="https://www.instagram.com/web_professionals/" class="social-in" title="Instagram für schöne Bilder" target="_blank">Instagram</a>
-        </div>
+        @include('_partials.socialmedia')
     </div>
+
 </div>
