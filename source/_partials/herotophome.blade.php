866d958eb0c209283bca6c32b1e930ae85eba4c0
Rolf Eggenberger <rolf@kunstwort.com>
2016-08-17 10:26:00 +0200

    Logo als SVG

--- source/_partials/herotophome.blade.php
+++ source/_partials/herotophome.blade.php
@@ -1,5 +1,6 @@
 <div class="row">
-    <div class="col s12 m12 l12 center-align webpro-hero-logo"><a href="/index.html"><img src="/images/logo_front.png" alt="Web Professionals Logo" title="Web Professionals Logo"></a></div>
+    <div class="col s12 m12 l12 center-align webpro-hero-logo"><a href="/index.html"><img src="/images/logo_web_professionals_top.svg" alt="Web Professionals Logo" title="Web Professionals Logo"></a></div>
+
     <div class="col s12 m6 l6 right-align webpro-hero-social">
         <div id="social-media">
             <a href="https://play.google.com/store/apps/details?id=com.techfabrik.webpro" class="social-android" title="Android App der Web Professionals herunterladen" target="_blank">Android</a>
