6dc54634eeaecb4607c002f51945a6f34b91e95c
Rolf Eggenberger <rolf@kunstwort.com>
2016-08-23 12:02:09 +0200

    schmea.org test

--- source/veranstaltungen/index.blade.php
+++ source/veranstaltungen/index.blade.php
@@ -53,18 +53,27 @@
             <div class="card z-depth-2 webpro-green small" itemscope itemtype="http://schema.org/Event">
                 <div class="card-stacked">
                     <div class="card-content white-text">
-                        <p>
                             <span class="card-events-day">Mittwoch</span><br>
-                            <span class="card-events-date" itemprop="startDate" content="2016-08-24T19:00">24. August 2016</span>
-                            <br>
-                            <span class="card-events-title" itemprop="name"><strong>Informationsanlass in Grabs SG (Techfabrik)</strong></span><br>
+                            <span class="card-events-date" itemprop="startDate" content="2016-08-24T19:00">24. August 2016</span><br>
                             19:00 - 20:00 Uhr<br>
-                            Techfabrik, Mühlbachstrasse 11a, 9472 Grabs
-                        </p>
+                            <br>
+                            <span class="card-events-title" itemprop="name"><strong>Informationsanlass in Grabs SG</strong></span><br>
+                            <span class="card-events-title" itemprop="description">Vorstellung der Kurse und Lehrgänge.</span><br>
+
+                        <br>
+                        <div itemscope itemtype="http://schema.org/LocalBusiness">
+                            <span itemprop="name"><strong>Techfabrik Grabs</strong></span><br>
+                            <div class="location" itemprop="location" itemscope itemtype="http://schema.org/Place">
+                                <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                                    <span itemprop="streetAddress">Mühlbachstrasse 11a</span>,
+                                    <span itemprop="postalCode">9472</span> <span itemprop="addressLocality">Grabs</span> (<span itemprop="addressRegion">SG</span>)
+                                </div>
+                                <a itemprop="hasMap" class="white-text" href="https://www.google.ch/maps/place/Techfabrik/@47.1805806,9.4487484,17z/data=!3m1!4b1!4m5!3m4!1s0x479b2fd2338a08f9:0x5bd6507cdbde9c3b!8m2!3d47.180577!4d9.4509371?hl=de" target="_blank"><i class="material-icons tiny">place</i> [Google Maps]</a>
+                            </div>
+                        </div>
                     </div>
                     <div class="card-action">
-                        <a class="white-text" href="https://www.google.ch/maps/place/Techfabrik/@47.1805806,9.4487484,17z/data=!3m1!4b1!4m5!3m4!1s0x479b2fd2338a08f9:0x5bd6507cdbde9c3b!8m2!3d47.180577!4d9.4509371?hl=de" target="_blank">Google Maps</a>
-                        <a href="https://www.facebook.com/events/1771274006420996/" target="_blank">Facebook Event</a>
+                        <a class="white-text" itemprop="url" href="https://www.facebook.com/events/1771274006420996/" target="_blank">Facebook Event</a>
                     </div>
                 </div>
             </div>
@@ -94,8 +103,8 @@
                 <div class="card-stacked">
                     <div class="card-content white-text">
                         <p>
-                            <span class="card-events-day">Montag</span><br>
-                            <span class="card-events-date" itemprop="startDate" content="2016-08-29T19:00">29. August 2016</span>
+                            <span class="card-events-day">Freitag</span><br>
+                            <span class="card-events-date" itemprop="startDate" content="2016-09-23T19:00">23. September 2016</span>
                             <br>
                             <span class="card-events-title" itemprop="name"><strong>Informationsanlass in Grabs SG (Techfabrik)</strong></span><br>
                             19:00 - 20:00 Uhr<br>
