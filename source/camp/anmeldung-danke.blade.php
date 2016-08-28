bdbd9e36d9a1a18de3a699939a69330022d40c47
Rolf Eggenberger <rolf@kunstwort.com>
2016-08-28 20:16:21 +0200

    Anmeldung fix

--- source/camp/anmeldung-danke.blade.php
+++ source/camp/anmeldung-danke.blade.php
@@ -49,8 +49,8 @@
         </div>
         <div class="col l7 m6 webpro-right">
             <p>
-                <strong>Herzlichen Dank für deine Anmeldung!</strong><br>
-                Du erhälst in den nächsten 1-2 Tagen eine Anmeldebestätigung per E-Mail sowie im Laufe der folgenden Tage weitere Unterlagen per Post. Wir freuen uns, dich bei den Web Professionals begrüssen zu dürfen!
+                <strong>Herzlichen Dank für deine Anmeldung zum #5 Web Professionals Barcamp 2016!</strong><br>
+                Du erhälst von unserer Seite keine weitere Anmeldebestätigung mehr zugesendet - wir freuen uns, dich am 29./30.10.2016 an unserem Barcamp begrüssen zu dürfen!
             </p>
         </div>
     </div>
