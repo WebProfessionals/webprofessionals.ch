438b2c9bd04c28b5bb0c89624d5484ed8abf8a36
Rolf Eggenberger <rolf@kunstwort.com>
2017-01-10 20:16:11 +0100

    Kurse updates

--- source/kurse/einfuehrung-in-laravel.blade.php
+++ source/kurse/einfuehrung-in-laravel.blade.php
@@ -42,7 +42,7 @@
                 ])
                 <nav class="webpro-nav-lvl2">
                     <a href="/kurse/auffrischungskurs-html-css/">HTML/CSS Auffrischung und Vertiefung</a><br>
-                    <a href="/kurse/hands-on-workshop-polymer-project/">Hands on workshop Polymer</a><br>
+                    <a href="/kurse/hands-on-workshop-polymer-project/">Hands-on Workshop Polymer</a><br>
                     <a href="/kurse/wordpress-themes-selber-programmieren/">WordPress Themes programmieren</a><br>
                     <a href="/kurse/einfuehrung-in-angularjs/">Einführung in AngularJS</a><br>
                     <a class="webpro-subnav-active" href="/kurse/einfuehrung-in-laravel/">Einführung in Laravel</a><br>
