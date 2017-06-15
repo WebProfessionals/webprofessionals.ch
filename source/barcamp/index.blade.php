ec5461ced3f902163df1bf7fee37234a7c6eaa63
Rolf Eggenberger <rolf@kunstwort.com>
2017-06-15 10:12:11 +0200

    Sponsoring update

--- source/barcamp/index.blade.php
+++ source/barcamp/index.blade.php
@@ -57,15 +57,7 @@
                 <a href="/barcamp/sponsoring/">Sponsoring</a><br>
             </nav>
             <br>
-            <p>Herzlichen Dank unseren <strong>Sponsoren</strong>, welche die Durchführung unseres Barcamps unterstützen!</p>
-            <p>Willst auch du unser <a href="/barcamp/sponsoring/">Barcamp und damit eine gute Sache unterstützen? Wir freuen uns auf deine Kontaktaufnahme!</a></p>
 
-            <div class="row">
-                <div class="col s12"><p class="sponsoren-titel">Hauptsponsor:</p></div>
-                <div class="col s8 m8 l8">
-                    <a href="http://www.viaduct.ch/" target="_blank"><img class="responsive-img" src="/images/barcamp/sponsoren/viaduct.png"></a>
-                </div>
-            </div>
 
             <span class="show-on-medium-and-up">
                 <!--<br><br><img class="responsive-img" src="/images/barcamp/logo-camp-5.png">-->
@@ -95,6 +87,20 @@
                 <a class="waves-effect waves-light btn webpro-red" href="/barcamp/anmeldung/">Jetzt anmelden</a>
             </section>
 
+            <br><br>
+
+            <h2 class="webpro">Sponsoren</h2>
+            <p>Herzlichen Dank unseren <strong>Sponsoren</strong>, welche die Durchführung unseres Barcamps unterstützen:</p>
+
+            <div class="row">
+                <div class="col s12"><p class="sponsoren-titel">Hauptsponsor:</p></div>
+                <div class="col s6 m6 l6">
+                    <a href="http://www.viaduct.ch/" target="_blank"><img class="responsive-img" src="/images/barcamp/sponsoren/viaduct.png"></a>
+                </div>
+            </div>
+            <br>
+            <p>Willst auch du unser <a href="/barcamp/sponsoring/">Barcamp und damit eine gute Sache unterstützen? Wir freuen uns auf deine Kontaktaufnahme!</a></p>
+            <br><br>
             <!--
             <div class="row">
                 <div class="col s12"><p class="sponsoren-titel">Getränke-Sponsor:</p></div>
