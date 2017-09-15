01543068b0919aba15a7ec86c568f4793262a5f4
Rolf Eggenberger <rolf@kunstwort.com>
2017-09-15 11:03:10 +0200

    front

--- source/index.blade.php
+++ source/index.blade.php
@@ -50,7 +50,7 @@
             </p>
             <p>
                 <strong>Letzte Infoanlässe vor Lehrgangsbeginn:</strong><br>
-                Mittwoch, 20. September 2017, 19 Uhr, <strong>ibW Chur</strong>, Zimmer 104<br>
+                Mittwoch, 20. September 2017, 19 Uhr, <strong>ibW Chur</strong>, Zimmer 302<br>
                 Freitag, 22. September 2017, 19 Uhr, <strong>Techfabrik Grabs</strong><br>
                 Dienstag, 26. September 2017, 19 Uhr, <strong>HSR Rapperswil</strong>, Zimmer 1.201
             </p>
