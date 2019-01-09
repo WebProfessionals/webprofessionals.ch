929724d13b59b92adb21e7f91ecf3569a49413bb
Rolf Eggenberger <rolf@kunstwort.com>
2019-01-09 14:03:59 +0100

    info

--- source/_partials/informationsanlaesse.blade.php
+++ source/_partials/informationsanlaesse.blade.php
@@ -1,7 +1,8 @@
 <section id="infoanlaesse" class="section scrollspy">
     <h2>Informationsanlässe:</h2>
     <ul class="webpro">
-        <li>- 18. Januar 2018, 19 Uhr, Startfeld, Lerchenfeldstrasse 3, 9014 St. Gallen</li>
+        <li>- 18. Januar 2019, 19 Uhr, Startfeld, Lerchenfeldstrasse 3, 9014 St. Gallen</li>
+        <li>- 02. März 2019, 13:30 Uhr, Gewerbepark "Schuhfabrik", Fabrikstrasse 24, 9472 Grabs</li>
     </ul>
     <p>Eine Anmeldung für die Informationsanlässe ist nicht notwendig. wir freuen uns aber natürlich sehr über eine kurze Nachricht auf info@web-professionals.ch falls ihr vorbei schauen werdet. Dies hilft uns bei der Vorbereitung von genügend Sitzplätzen :-)</p>
     <p>Weitere Termine (Grabs, Rapperswil, Chur) sind derzeit in Abklärung und werden demnächst hier publiziert.</p>
