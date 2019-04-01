cd41c1629ce7ea260274021098614a0199b46b83
Rolf Eggenberger <rolf@kunstwort.com>
2019-04-01 11:22:07 +0200

    Info

--- source/_partials/informationsanlaesse.blade.php
+++ source/_partials/informationsanlaesse.blade.php
@@ -1,9 +1,8 @@
 <section id="infoanlaesse" class="section scrollspy">
     <h2>Nächste Informationsanlässe:</h2>
     <ul class="webpro-list">
-        <!--<li>02. März 2019, 13:30 Uhr, Gewerbepark "Schuhfabrik", Fabrikstrasse 24, 9472 Grabs <a href="https://goo.gl/maps/prTrCyyjt5B2" target="_blank">Google Maps</a> | <a href="https://www.facebook.com/events/397886564295481/" target="_blank">Zusagen via Facebook-Event</a></li>-->
-        <li>04. März 2019, 19:00 Uhr, ibW Höhere Fachschule Südostschweiz, Zimmer 401, Bahnhofpark 2a, 7320 Sargans - <a href="https://goo.gl/maps/6tEWxdjSS5A2" target="_blank">Google Maps</a> | <a href="https://www.facebook.com/events/397467371013949/" target="_blank">Zusagen via Facebook-Event</a></li>
         <li>06. April 2019, 10:00 Uhr, Startfeld, Lerchenfeldstrasse 3, 9014 St.Gallen - <a href="https://goo.gl/maps/DkNoJZDdBg72" target="_blank">Google Maps</a> | <a href="https://www.facebook.com/events/332371147383105/" target="_blank">Zusagen via Facebook-Event</a></li>
+        <li>13. April 2019, 13:30 Uhr, Gewerbepark "Schuhfabrik", Fabrikstrasse 24, 9472 Grabs <a href="https://goo.gl/maps/prTrCyyjt5B2" target="_blank">Google Maps</a></li>
     </ul>
     <p>Eine Anmeldung für die Informationsanlässe ist nicht notwendig. wir freuen uns aber natürlich sehr über eine kurze Nachricht auf info@web-professionals.ch falls ihr vorbei schauen werdet. Dies hilft uns bei der Vorbereitung von genügend Sitzplätzen :-)</p>
     <p>Zur Info: in Rapperswil findet kein Informationsanlass statt. Wir würden uns sehr freuen, Dich an einem unserer Anlässe in Grabs, St.Gallen, Chur oder Sargans begrüssen oder deine Fragen telefonisch beantworten zu dürfen.</p>
