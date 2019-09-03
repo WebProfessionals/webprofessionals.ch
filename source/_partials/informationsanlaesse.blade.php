657f199520b0095a69ab85bf5cb981e520a67c0c
Rolf Eggenberger <rolf@kunstwort.com>
2019-09-03 16:33:09 +0200

    Infoanlass

--- source/_partials/informationsanlaesse.blade.php
+++ source/_partials/informationsanlaesse.blade.php
@@ -1,7 +1,7 @@
 <section id="infoanlaesse" class="section scrollspy">
     <h2>Nächste Informationsanlässe:</h2>
     <ul class="webpro-list">
-        <li>17. August 2019, 13:00 Uhr, Gewerbepark "Schuhfabrik", Fabrikstrasse 24, 9472 Grabs - <a href="https://goo.gl/maps/prTrCyyjt5B2" target="_blank">Google Maps</a> <!--| <a href="https://www.facebook.com/events/332371147383105/" target="_blank">Zusagen via Facebook-Event</a>--></li>
+        <li>30. September 2019, 19:00 Uhr, Gewerbepark "Schuhfabrik", Fabrikstrasse 24, 9472 Grabs - <a href="https://goo.gl/maps/prTrCyyjt5B2" target="_blank">Google Maps</a> <!--| <a href="https://www.facebook.com/events/332371147383105/" target="_blank">Zusagen via Facebook-Event</a>--></li>
     </ul>
     <p>Eine Anmeldung für die Informationsanlässe ist nicht notwendig. wir freuen uns aber natürlich sehr über eine kurze Nachricht auf info@web-professionals.ch falls ihr vorbei schauen werdet. Dies hilft uns bei der Vorbereitung von genügend Sitzplätzen :-)</p>
     <p>Zur Info: in Rapperswil findet kein Informationsanlass statt. Wir würden uns sehr freuen, Dich an einem unserer Anlässe in Grabs, St.Gallen, Chur oder Sargans begrüssen oder deine Fragen telefonisch beantworten zu dürfen.</p>
