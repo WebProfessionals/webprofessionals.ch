03796f2eab7c4c1bc5e4a3e667bac7c05828f45a
Rolf Eggenberger <rolf@kunstwort.com>
2019-10-16 15:10:55 +0200

    content

--- source/_partials/informationsanlaesse.blade.php
+++ source/_partials/informationsanlaesse.blade.php
@@ -1,7 +1,7 @@
 <section id="infoanlaesse" class="section scrollspy">
     <h2>Nächste Informationsanlässe:</h2>
     <ul class="webpro-list">
-        <li>30. September 2019, 19:00 Uhr, Gewerbepark "Schuhfabrik", Fabrikstrasse 24, 9472 Grabs - <a href="https://goo.gl/maps/prTrCyyjt5B2" target="_blank">Google Maps</a> <!--| <a href="https://www.facebook.com/events/332371147383105/" target="_blank">Zusagen via Facebook-Event</a>--></li>
+        <li>22. Oktober 2019, 18:00 Uhr, Liip AG, Neumarkt 5, 3. Stock, 9000 St.Gallen - <a href="https://www.google.com/maps/place/Liip+AG/@47.4217033,9.3692739,17z/data=!3m1!4b1!4m5!3m4!1s0x479b1e4b98a711c1:0x369b70f01bcdd979!8m2!3d47.4217033!4d9.3714626" target="_blank">Google Maps</a> <!--| <a href="https://www.facebook.com/events/332371147383105/" target="_blank">Zusagen via Facebook-Event</a>--></li>
     </ul>
     <p>Eine Anmeldung für die Informationsanlässe ist nicht notwendig. wir freuen uns aber natürlich sehr über eine kurze Nachricht auf info@web-professionals.ch falls ihr vorbei schauen werdet. Dies hilft uns bei der Vorbereitung von genügend Sitzplätzen :-)</p>
     <p>Zur Info: in Rapperswil findet kein Informationsanlass statt. Wir würden uns sehr freuen, Dich an einem unserer Anlässe in Grabs, St.Gallen, Chur oder Sargans begrüssen oder deine Fragen telefonisch beantworten zu dürfen.</p>
